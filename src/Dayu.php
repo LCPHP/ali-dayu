ac723ad26e12092ef039e1461961df9e5443bdbf
lucong <332553882@qq.com>
2017-02-07 16:04:01 +0800

    ad

---
+++ src/Dayu.php
@@ -0,0 +1,51 @@
+<?php
+namespace niklaslu;
+class Dayu {
+    
+    private $appkey = NULL;
+    private $secret = NULL;
+    private $client = NULL;
+    
+    public function __construct($config){
+        
+        require_once 'sdk/TopSdk.php';
+        
+        $client = new \TopClient();    
+        $this->appkey = $config['appkey'];
+        $this->secret = $config['secret'];
+        
+        $client->appkey = $this->appkey;
+        $client->secretKey = $this->secret;
+        $client->format = 'json';
+        
+        $this->client = $client;
+        
+    }
+    
+    public function smsSend($data){
+        
+        $req = new  \AlibabaAliqinFcSmsNumSendRequest();
+        $req->setExtend($data['extend']);
+        $req->setSmsType($data['sms_type']);
+        $req->setSmsFreeSignName($data['sms_free_sign_name']);
+        
+        $smsParam = json_encode($data['sms_param']);
+        $req->setSmsParam($smsParam);
+        $req->setRecNum($data['rec_num']);
+        $req->setSmsTemplateCode($data['sms_template_code']);
+        
+        $resp = $this->client->execute($req);
+        return $this->returnData($resp);
+    }
+    
+    public function returnData($data){
+        
+        if (isset($data->result)){
+            return ['status' => 1 , 'data' => $data->result];
+        }
+        
+        if (isset($data->code)){
+            return ['status' => 0 , 'data'=> $data , 'msg'=>$data->sub_msg];
+        }
+    }
+}
---
+++ src/demo/sms.php
@@ -0,0 +1,28 @@
+<?php
+use niklaslu\Dayu;
+require_once 'ali-dayu/src/Dayu.php';
+
+$config = [
+  'appkey' => '23317582',
+  'secret' => 'db7e5a15c107b0ee76cee7d384c912ec'
+];
+
+$Dayu = new Dayu($config);
+
+$smsData = [
+    'extend' => '123456',
+    'sms_type' => 'normal',
+    'sms_free_sign_name' => 'warm家公寓',
+    'sms_param' => [
+        'address' => '测试地址',
+        'date' => '今天',
+        'date2' => '昨天'
+    ],
+    'rec_num' => '18676669410',
+    'sms_template_code' => 'SMS_6340224'
+];
+
+$template = '${address}于${date}申请退/转租，请于${date2}完成资料审核。';
+$result = $Dayu->smsSend($smsData);
+
+var_dump($result);
